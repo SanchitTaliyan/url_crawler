282bb3eb37c7ba49ec59ea5c7f7fb912a89fd9ac
SanchitTaliyan <taliyan.1@iitj.ac.in>
2024-07-28 12:49:07 +0530

    Added support for both single url and multiple urls

--- app/controllers/api/v1/crawl_pages_controller.rb
+++ app/controllers/api/v1/crawl_pages_controller.rb
@@ -1,10 +1,10 @@
 module Api
   module V1
     class CrawlPagesController < ApplicationController
-      skip_before_action :verify_authenticity_token, only: :create
+      skip_before_action :verify_authenticity_token, only: [:crawl_pages, :crawl_page]
 
 
-      def create
+      def crawl_pages
         urls = params[:urls]
         results = urls.map do |url|
           crawler = CrawlerService.new(url)
@@ -15,6 +15,16 @@ module Api
 
         render json: results, status: :ok
       end
+
+      def crawl_page
+        url = params[:url]
+        crawler = CrawlerService.new(url)
+        metadata = crawler.fetch_metadata
+        topics = crawler.classify_content(metadata[:body])
+
+        render json: { metadata: metadata, topics: topics }, status: :ok
+      end
+
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :crawl_pages, only: [:create]
+      post 'crawl_pages', to: 'crawl_pages#crawl_pages'
+      post 'crawl_page', to: 'crawl_pages#crawl_page'
     end
   end
 end
